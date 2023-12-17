93b39f44adf3f644e260cdb9b90a384e0c60276e
Your Name <kumarvipin897946@gmail.com>
2023-12-17 09:31:22 +0530

    Add project files.

---
+++ MVCActionResults/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCActionResults.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult Resister()
+        {
+            return View();
+        }
+        public ViewResult ForgotPassword()
+        {
+            return View("ForgotPwd");
+        }
+        public ViewResult ResetPassword()
+        {
+            return View("~/Views/Home/ResetPwd.cshtml");
+        }
+        public ViewResult Contact()
+        {
+            return View();
+        }
+        public ViewResult Mission()
+        {
+            return View("~/Views/Test/Mission.cshtml");
+        }
+
+    }
+}
